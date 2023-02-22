9dda2c489f7f4bbb2f33f363706b649dbe9508b7
RangnathSM <rangnathpalegar@gmail.com>
2023-02-22 14:20:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Children from './Components/children'
+
+function App() {
+  
+  return (
+    <div className="App">
+    <Children/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/children.jsx
@@ -0,0 +1,36 @@
+import Card from 'react-bootstrap/Card';
+import {useState, useEffect} from 'react'
+function TextExample() {
+    let [child, setChild]= useState([])
+
+    useEffect(()=>{
+        let fetchData= async() =>{
+        let response =await fetch(`https://www.reddit.com/r/reactjs.json`)
+        let Conte=await response.json()
+        setChild(Conte.data.children)
+    }
+    fetchData()
+    },[child])
+
+
+  return (
+    <div className="container " style={{display:"flex",flexWrap:"wrap",paddingTop:"30px"}} >
+        {child.map((item)=>(
+            <div className="cards">
+                <Card style={{ width: '400px',marginRight:"30px",backgroundColor:"black",color:"white"}}>
+      <Card.Body>
+        <Card.Title>Title :{item.data.title}</Card.Title>
+        <Card.Text><b>Selftext_html :</b>{item.data.selftext_html} </Card.Text>
+        <Card.Link href="/"><b>Url :</b>{item.data.url}</Card.Link>
+        <Card.Text><b>Score :</b>{item.data.score}</Card.Text>      
+      </Card.Body>
+    </Card>
+            </div>
+
+        ))}
+        
+    </div>
+  );
+}
+
+export default TextExample;
